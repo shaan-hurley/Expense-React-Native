ef57f2f3d953234ccb34723137fbb99d4f7a098a
shaannessy25 <shaan.hurley25@gmail.com>
2020-11-16 10:20:12 -0800

    adding stack navigator

---
+++ screens/HomeScreen.js
@@ -0,0 +1,25 @@
+
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      {/* <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      /> */}
+    </View>
+  );
+}
+
+// function HomeScreen() {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Home Screen</Text>
+//       </View>
+//     );
+//   }
