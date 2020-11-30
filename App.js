22f1c088d27769b2e883d877073fc1fadf6c7fec
shaannessy25 <shaan.hurley25@gmail.com>
2020-11-30 05:01:18 -0800

    added bottom tab navigation and icons, chart icon not working

--- App.js
+++ App.js
@@ -1,38 +1,49 @@
 // In App.js in a new project
 
-import * as React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen'
 import DetailsScreen  from './screens/DetailsScreen'
 import Chart from './screens/ChartScreen'
+import { Ionicons } from 'react-native-vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
+  const [ query, setQuery ] = useState('')
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen 
-        name="Home" 
-        component={HomeScreen} 
-        options={{ 
-          title: "Expenses",
-          headerStyle: {
-            backgroundColor: '#f4511e'
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-home' : 'ios-home';
+            } else if (route.name === 'Chart') {
+              iconName = focused ? 'ios-bar-chart' : 'ios-chart';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'ios-person' : 'ios-person'
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={32} color={color} />;
           },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 24
-          }
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
         }}
-      />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Chart" component={Chart} />
-      </Stack.Navigator>
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Chart" component={Chart} />
+        {/* <Tab.Screen name="Details" component={DetailsScreen} /> */}
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,22 @@
 
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, SafeAreaView, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from 'react-native-vector-icons'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph
+} from 'react-native-chart-kit'
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen( ) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Expense</Text>
+      {/* <Button
         title="Go to Details"
         onPress= { () => {
             navigation.navigate('Details', {
@@ -17,8 +25,36 @@ export default function HomeScreen({ navigation }) {
             })
         }}
       />
-      <Button title="Go to charts" onPress={() => navigation.navigate('Chart')} />
-    </View>
+      <Button title="Go to charts" onPress={() => navigation.navigate('Chart')} /> */}
+    </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+  },
+  title: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 35,
+    fontWeight: 'bold',
+    letterSpacing: 3,
+    padding: (0, 20),
+    backgroundColor: "#1abc9c",
+    color: "white",
+    width: "100%",
+    
+  },
+  list: {
+    flex: 1,
+    width: "100%"
+  },
+  input: {
+    width: '100%', 
+    borderWidth: 1,
+    padding: 10
+  },
+});
